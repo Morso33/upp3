51adae2b7e944c695eb3a819046b3a65598f3cca
Morso <69962221+Morso33@users.noreply.github.com>
2021-02-18 14:28:54 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,107 @@
+let x = 1
+let y = 10
+let z = 0
+let h = 1
+let rad = 11
+let runtime = 0;
+let frukter = ["Achacha", "Ananas", "Cherimoya", "Guanabana"];
+var codelang = "JS, PHP, HTML, Python";
+
+
+
+if (rad == 0){
+
+console.log("värdet är noll")
+    
+}
+
+if (rad == 1){
+
+    console.log("Endast en rad")
+    
+}
+
+
+
+while (rad > 0, rad < 10){
+
+
+        console.log("detta är rad" + rad)
+    
+    
+}
+
+
+if (rad > 0 && rad > 10){
+  console.log("För mycket rader eller ogiltigt värde")
+}
+
+
+
+while(runtime == 0){
+    console.log(frukter)
+    runtime++
+}
+
+
+
+
+
+
+
+//for (y > 10; y < 20;){
+
+
+  //  y = y + 1
+
+
+//}
+
+
+
+//while (z > 0, z < 20){
+
+  //  if ( z % 2 == 0) {
+  //      console.log (z)
+ //       z = z+1
+ //   }
+
+  //  else{
+  //      z = z + 1
+  //      console.log("no")
+  //  }
+
+
+//}
+
+
+//while (true){
+
+
+
+//if (x <= 20){
+
+ //   console.log("hej på dig rad " + x)
+ //   x = x + 1
+
+//}
+
+//}
+
+
+
+while (h > 0 && h < 10){
+
+    console.log(h)
+    h++
+
+
+
+}
+
+
+
+
+
+console.log(codelang.split(" "));
+console.log(frukter.sort());
